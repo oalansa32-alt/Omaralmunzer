ec9962642e02ad965ba6934d020e584658d26f53
oalansa32-alt <oalansa32@gmail.com>
2026-05-08 23:11:11 +0200

    Add files via upload

---
+++ SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.maqaree;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // الانتظار 2 ثانية ثم الانتقال إلى MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000); // 2000 ميلي ثانية = 2 ثانية
+    }
+}
